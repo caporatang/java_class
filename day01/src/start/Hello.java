74d6d0ad3ca2a69a6be65e99c826f2609cbd886b
caporatang <86291514+caporatang@users.noreply.github.com>
2021-06-22 18:05:45 +0900

    Add files via upload

---
+++ day01/src/start/Hello.java
@@ -0,0 +1,14 @@
+package start;//항상 맨 위에는 package이름이 와야한다.
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// main함수에 처리내용을 넣어보자.
+		// syso , sysout + 컨트롤 + 스페이스바 
+		System.out.println("나는 자동완성입니다.!!!");
+		System.out.println("환영합니다.");
+		System.out.print("반갑습니다.");
+		
+	}
+
+}
---
+++ day01/src/start/hi.java
@@ -0,0 +1,13 @@
+package start;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// 
+		System.out.println("안녕하세요");
+		System.out.println("코딩");
+		System.out.println("자바");
+
+	}
+
+}
