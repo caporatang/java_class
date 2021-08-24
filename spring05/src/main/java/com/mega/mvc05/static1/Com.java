cc90a93d50711650214a81887618672b5a5d01e6
TAE IL <86291514+caporatang@users.noreply.github.com>
2021-08-24 20:38:08 +0900

    Add files via upload

---
+++ spring05/src/main/java/com/mega/mvc05/static1/Com.java
@@ -0,0 +1,26 @@
+package com.mega.mvc05.static1;
+
+public class Com {
+
+	String name;
+	int age;
+	String gender;
+
+	static int count;
+	static int ages;
+
+	public Com(String name, int age, String gender) {
+
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		count++;
+		ages += age;
+	}
+
+	@Override
+	public String toString() {
+		return "Com [name=" + name + ", age=" + age + ", gender=" + gender + "]";
+	}
+
+}
---
+++ spring05/src/main/java/com/mega/mvc05/static1/Day.java
@@ -0,0 +1,39 @@
+package com.mega.mvc05.static1;
+
+public class Day {
+
+	// 맴버변수, 전역 변수, 자동 초기화
+	// 자동초기화는 배열, 전역변수, 맴버 변수 밖에 없다
+	String doing; // null
+	int time; // 0
+	String location;// null new 할때마다 doing , time , location은 복하된다.
+
+	static int sum;
+	static int count; // static 변수는 new해도 새로 생성되지 않는다
+
+	public Day(String doing, int time, String location) {
+
+		this.doing = doing; // this는 위에서 선언 한 전역 변수를 의미한다.
+		this.time = time;
+		this.location = location;
+		count++;
+		sum += time;
+	}
+	// static 메서드는 객체 생성하지 않아도 아무때나 클래스 이름으로 
+	// 접근해서 그 기능을 처리하게 할 수 있다. 
+	// 자주 쓰는 기능은 static 메서드로 만들어 놓으면 아무때나 쓸 수 있어서 편하다.
+	
+	public static int getAvg() {
+		return sum / count;
+		// static 메서드 안에는 static변수만 사용 가능!!! instance변수 사용 불가능 !!
+		// instance변수는 객체 생성후 접근 가능하기 때문이다
+		// static메서드는 객체 생성하지 않아도 접근해서 처리를 해야한다.
+		// return doing;
+	}
+
+	@Override
+	public String toString() {
+		return "day [doing=" + doing + ", time=" + time + ", location=" + location + "]";
+	}
+
+}
